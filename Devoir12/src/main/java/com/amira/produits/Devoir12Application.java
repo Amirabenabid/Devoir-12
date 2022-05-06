f810ce9fe8614f913fefa9eb98116abad9025add
Amira Ben Abid <91790113+Amirabenabid@users.noreply.github.com>
2022-05-06 20:59:28 +0100

    Add files via upload

---
+++ Devoir12/src/main/java/com/amira/produits/Devoir12Application.java
@@ -0,0 +1,13 @@
+package com.amira.produits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devoir12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devoir12Application.class, args);
+	}
+
+}
